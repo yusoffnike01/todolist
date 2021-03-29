f95600f245dac20234367ef1ca55d6c116cc09dc
yusoffnike01 <ronaldoyusoff@gmail.com>
2021-03-29 23:49:48 +0800

    add login and register form

--- todos/urls.py
+++ todos/urls.py
@@ -3,7 +3,10 @@ from . import views
 urlpatterns=[
     path('list/',views.list_todo_item),
     path('insert_todo/',views.insert_todo_item, name='insert_todo_item'),
-    path('delete_todo/<int:todo_id>',views.delete_todo_item, name='delete_todo_item')
+    path('delete_todo/<int:todo_id>',views.delete_todo_item, name='delete_todo_item'),
+    path('login/',views.login_user,name='login'),
+    path('register/',views.register_user,name='register_user'),
+
 
 
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
 from .models import Todo
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def list_todo_item(request):
@@ -16,6 +18,26 @@ def delete_todo_item(request, todo_id):
     todo_to_delete=Todo.objects.get(id=todo_id)
     todo_to_delete.delete()
     return redirect('/todos/list/')
+
+def register_user(request):
+    if request.method =='POST':
+        form=UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully')
+            return redirect('login')
+    else:
+        form=UserCreationForm()
+    return render(request,'todos/register.html',{'form': form})
+
+def login_user(request):
+    if request.method=='POST':
+        form=AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            return redirect('/todos/list/')
+    else:
+        form=AuthenticationForm()
+    return render(request,'todos/login.html',{'form': form})
 
 
 
